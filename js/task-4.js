5916e85260903dc714192bcc797a92887fffd7d8
Olexandr <63474923+alexandr90h@users.noreply.github.com>
2020-09-17 13:55:56 +0300

    4 task

--- js/task-4.js
+++ js/task-4.js
@@ -1,4 +1,14 @@
+const btnAction = document.querySelectorAll('button');
+// const btnIncrement = document.querySelector();
+
 const valueElem = document.querySelector('#value');
-let counterValue = valueElem.textContent;
-const increment = val => (val += 1);
-const decrement = val => (val -= 1);
+let counterValue = Number(valueElem.textContent);
+const increment = function (val) {
+  return (val += 1);
+};
+const decrement = function () {
+  counterValue -= 1;
+  console.log(counterValue);
+};
+console.log(btnAction);
+btnAction[0].addEventListener('click', decrement());
